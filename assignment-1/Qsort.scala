b0c72598e076346ad7004c7b62c258d4ba8757cc
nandadeep <aditya.nandadeep@gmail.com>
2018-09-27 15:21:36 -0700

    finish

--- assignment-1/Qsort.scala
+++ assignment-1/Qsort.scala
@@ -1,78 +1,97 @@
 class Utilities {
 
-  def swap(a: Array[Int], i: Int, j: Int) {
-    if (i == j) {
-      return
-    } else {
-      var temp: Int = a(i)
-      a(i) = a(j)
-      a(j) = temp
-      return a
-    }
+  def swap(arr: Array[Int], i: Int, j: Int): Array[Int] = {
+    // make a new array with updated elements and return that.
+    var new_arr = arr
+    var temp = new_arr(i)
+    new_arr(i) = new_arr(j)
+    new_arr(j) = temp
+    new_arr
   }
 
   def createArray(n: Int): Array[Int] = {
-    return scala.util.Random.shuffle(1 to n).toArray
+    scala.util.Random.shuffle(1 to n).toArray
   }
 
   def partition(arr: Array[Int], low: Int, high: Int): Int = {
 
     var pivot = arr(high)
     var middle = low
-
+    var swapped_arr = arr
     for (i <- low to high) {
       if (arr(i) < pivot) {
-        var temp = arr(i)
-        arr(i) = arr(middle)
-        arr(middle) = temp
+        swapped_arr = swap(arr, i, middle)
         middle += 1
       }
     }
-    var temp = arr(high)
-    arr(high) = arr(middle)
-    arr(middle) = temp
-    return middle
+    swapped_arr = swap(swapped_arr, high, middle)
+    middle
   }
-  
-  def bubbleSort(arr: Array[Int], low: Int, high: Int):Array[Int] = {
-    // var arr_copy = arr
-    for(i <- low to high){
-      for(j <- i + 1 to high){
-        if(arr(i) > arr(j)){
-          var temp = arr(i)
-          arr(i) = arr(j)
-          arr(j) = temp
+
+  def bubbleSort(arr: Array[Int], low: Int, high: Int): Array[Int] = {
+    var swapped_arr = arr
+    for (i <- low to high) {
+      for (j <- i + 1 to high) {
+        if (arr(i) > arr(j)) {
+          swapped_arr = swap(arr, i, j)
         }
       }
     }
-    return arr
+    swapped_arr
   }
-  
-  def quickSort(arr: Array[Int], low: Int, high: Int) : Array[Int] = {
-    if((high - low) < 10){
+
+  def quickSort(arr: Array[Int], low: Int, high: Int): Array[Int] = {
+    if ((high - low) < 10) {
       return bubbleSort(arr, low, high)
     } else {
       var middle = partition(arr, low, high)
-      if(low < middle){
+      if (low < middle) {
         quickSort(arr, low, middle - 1)
       }
-      if(middle < high){
+      if (middle < high) {
         quickSort(arr, middle + 1, high)
       }
     }
-    return arr
+    arr
   }
 
+  def printArray(arr: Array[Int], n: Int) {
+
+    println(arr.mkString(" "))
+
+  }
+
+  def verifyArray(arr: Array[Int], n: Int) {
+
+    for(i <- 0 to n - 2) {
+      if(arr(i) > arr(i + 1)) {
+        println("failed")
+      }
+    }
+    println("verified")
+  }
 }
 
+object Qsort {
 
-var ut = new Utilities
-var arr = ut.createArray(10)
-println("before")
-arr.foreach(a => println(a))
-println("----------------")
-println("after")
-// var part = ut.partition(arr, 0, 9)
-// println(part)
-var sorted_arr = ut.quickSort(arr, 0, 9)
-sorted_arr.foreach(a => println(a))
+  // wrapping the qsort in an object. 
+
+  def main(args: Array[String]) {
+    // args.foreach(a => print(a))
+
+    var nums: Int = args(0).toInt
+    var ut = new Utilities
+    var numbers = ut.createArray(nums)
+    println("before sorting: ")
+    println(numbers.mkString(" "))
+    var sorted_numbers = ut.quickSort(numbers, 0, nums - 1)
+    println("after sorting: ")
+    println(sorted_numbers.mkString(" "))
+
+    ut.verifyArray(sorted_numbers, nums)
+    
+
+  }
+}
+ 
+
