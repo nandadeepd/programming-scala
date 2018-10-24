d4fe49890fd23ee1f0b85c668b69a2391481d791
Nandadeep Davuluru <davuluru@augusta.cs.pdx.edu>
2018-10-23 23:57:52 -0700

    code wrap

--- assignment-4/recursion.c
+++ assignment-4/recursion.c
@@ -8,79 +8,88 @@
 // factorial function
 
 int fac(int n) {
-  if (n <= 1) 
-    return 1;
-  return n * fac(n-1);
+	if (n <= 1) 
+		return 1;
+	return n * fac(n-1);
 }
 
 int facTR(int n) {
-  int helper(int k, int acc) {
-    if (k <= 1)
-      return acc;
-    return helper(k-1, k*acc);
-  }
-  return helper(n, 1);
+	int helper(int k, int acc) {
+		if (k <= 1)
+			return acc;
+		return helper(k-1, k*acc);
+	}
+	return helper(n, 1);
 }
 
 // unknown function g
 
 int g(int n) {
-  if (n <= 3) 
-    return 1;
-  else return g(n-1) + f(n-2) + f(n-3);
+	if (n <= 3) 
+		return 1;
+	else return g(n-1) + g(n-2) + g(n-3);
 }
 
 int gTR(int n) {
 
-  int helperGT(int n, int m, int p) {
-    if (n <= 3) return 1;
-    else return helperGT(n - 1, n - 2, n - 3)
-  }
+	int helper(int n, int a, int b, int c) {
+		if (n <= 3) return a;
+		return helper(n-1, a+b+c, a, b);
 
-  return helperGT(n - 1, n - 2, n - 3)
-  
+	}
+	return helper(n, 1, 1, 1);
+
 
 }
 
 // unknown function h
 
 int h(int n) {
-  if (n <= 1)
-    return 1;
-  if (n%2 == 0) 
-    return h(n/2) + n;
-  else
-    return h(n-1) + 1;
+	if (n <= 1)
+		return 1;
+	if (n%2 == 0) 
+		return h(n/2) + n;
+	else
+		return h(n-1) + 1;
 }
 
 int hTR(int n) {
+	int helper(int m, int acc) {
 
-  // ... add code ...
+		if (m <= 1) 
+			return acc;
+		if (m % 2 == 0) 
+			return helper(m / 2 , m + acc );
+		else 
+			return helper(m - 1, acc + 1);
+
+	}
+	return helper(n, 1);
 
 }
 
 int main() {
-  printf("fac(1-10)   = ");
-  for (int i=1; i<10; i++) printf("%d, ", fac(i));
-  printf("%d\n", fac(10));
+	printf("fac(1-10)   = ");
+	for (int i=1; i<10; i++) printf("%d, ", fac(i));
+	printf("%d\n", fac(10));
 
-  printf("facTR(1-10) = ");
-  for (int i=1; i<10; i++) printf("%d, ", facTR(i));
-  printf("%d\n", facTR(10));
+	printf("facTR(1-10) = ");
+	for (int i=1; i<10; i++) printf("%d, ", facTR(i));
+	printf("%d\n", facTR(10));
 
-  printf("g(1-10)   = ");
-  for (int i=1; i<10; i++) printf("%d, ", g(i));
-  printf("%d\n", g(10));
+	printf("g(1-10)   = ");
+	for (int i=1; i<10; i++) printf("%d, ", g(i));
+	printf("%d\n", g(10));
 
-  printf("gTR(1-10) = ");
-  for (int i=1; i<10; i++) printf("%d, ", gTR(i));
-  printf("%d\n", gTR(10));
+	printf("gTR(1-10) = ");
+	for (int i=1; i<10; i++) printf("%d, ", gTR(i));
+	printf("%d\n", gTR(10));
 
-  printf("h(1-10)   = ");
-  for (int i=1; i<10; i++) printf("%d, ", h(i));
-  printf("%d\n", h(10));
+	printf("h(1-10)   = ");
+	for (int i=1; i<10; i++) printf("%d, ", h(i));
+	printf("%d\n", h(10));
 
-  printf("hTR(1-10) = ");
-  for (int i=1; i<10; i++) printf("%d, ", hTR(i));
-  printf("%d\n", hTR(10));
+	printf("hTR(1-10) = ");
+	for (int i=1; i<10; i++) printf("%d, ", hTR(i));
+	printf("%d\n", hTR(10));
 }
